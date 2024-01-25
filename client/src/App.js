8bb2ae405b799f8e2d380f3cf6f94939bba68b96
abu <mohamedaboobackerp@gmail.com>
2024-01-25 17:08:26 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import './App.css';
+import bgimg from './assets/bgimg.jpg'
+function App() {
+  return (
+    <div className="App">
+      <div class="d-grid gap-2">
+        <button
+          type="button"
+          name=""
+          id=""
+          class="goBtn border-0 rounded-2 "
+        >
+          LetsGo...!
+        </button>
+      </div>
+      
+
+      <h1></h1>
+    </div>
+  );
+}
+
+export default App;
