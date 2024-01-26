70570698d4cd8b1439698afd5f3a09238b180eb7
abu <mohamedaboobackerp@gmail.com>
2024-01-26 18:23:31 +0530

    add column added

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -3,17 +3,17 @@ import logoImg from './logo.png'
 export default function Navbar() {
     return (
         <>
-            <nav className="navbar fixed-top " style={{backgroundColor:' #a2acb6',fontWeight: '800'}}>
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
+            <nav className="navbar fixed-top " style={{backgroundColor:' #8c8c8c80',fontWeight: '800',width:'21%',minWidth:'200px',borderBottomRightRadius:'47px'}}>
+                <div className="container-fluid justify-content-center ">
+                    <a className="navbar-brand d-flex align-items-center " href="#">
                         <img
                             src={logoImg}
                             alt="Logo"
-                            width={30}
-                            height={30}
+                            width={40}
+                            height={40}
                             className="d-inline-block align-text-top"
                         />
-                        VehicleRC
+                        <span className='m-2'>VehicleRC</span>
                     </a>
                 </div>
             </nav>
--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -10,7 +10,7 @@ const vehicleData = [
     model: "2016",
     number: "KL 71 B 7909",
     insuranceDate: "22/12/2024",
-    pcc: "22/06/2024 (148 days)",
+    pcc: "22/06/2025 ",
     backgroundImage: 'https://media.zigcdn.com/media/model/2023/Jun/lest-side-view-1341726075_600x400.jpg'
   },
   {
@@ -19,7 +19,7 @@ const vehicleData = [
     model: "2009",
     number: "KL 51 A 1923",
     insuranceDate: "22/01/2024",
-    pcc: "22/06/2024 (148 days)",
+    pcc: "22/06/2024 ",
     backgroundImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Honda_CBZ.jpg/300px-Honda_CBZ.jpg'
   },
   {
@@ -28,7 +28,7 @@ const vehicleData = [
     model: "2019",
     number: "KL 49 L 1903",
     insuranceDate: "22/12/2024",
-    pcc: "22/06/2024 (148 days)",
+    pcc: "22/06/2024 ",
     backgroundImage: 'https://imgd.aeplcdn.com/1280x720/n/cw/ec/55973/rs-200-left-side-view.png'
   },
   {
@@ -37,7 +37,7 @@ const vehicleData = [
     model: "2022",
     number: "KL 6Q Q 8005",
     insuranceDate: "09/02/2024",
-    pcc: "22/06/2024 (148 days)",
+    pcc: "22/06/2024 ",
     backgroundImage: 'https://stimg.cardekho.com/images/carexteriorimages/930x620/Maruti/S-Presso/10348/Maruti-S-Presso-LXi/1687519307943/front-left-side-47.jpg'
   },
 ];
@@ -59,6 +59,8 @@ export default function HomePage() {
         <div className="homeMain">
           {vehicleData.map((card, index) => {
             const remainingDays = getRemainingDays(card.insuranceDate);
+            const remainingDaysOfPcc = getRemainingDays(card.pcc);
+            console.log(remainingDaysOfPcc, 'pcc')
             const insuranceStyle = remainingDays < 1 ? { color: 'red' } :
               remainingDays < 15 ? { color: '#bd7a00' } : {};
             return (
@@ -72,7 +74,7 @@ export default function HomePage() {
                     <h6 style={insuranceStyle}>
                       Insurance: <b>{card.insuranceDate} ({remainingDays} days)</b>
                     </h6>
-                    <h6>Pcc : <b>{card.pcc}</b></h6>
+                    <h6>Pcc : <b>{card.pcc} ({remainingDaysOfPcc} days)</b></h6>
                   </div>
 
                   <div className='actions'>
@@ -87,6 +89,26 @@ export default function HomePage() {
               </div>
             );
           })}
+          <div className="card col-3" style={{ width: "20rem", minHeight: '256px', margin: '30px', backgroundImage: `url(https://t4.ftcdn.net/jpg/05/53/55/21/360_F_553552160_9HxeyvvbSrOtEqpaiWsCD2TEnwQxvrwB.jpg)` }}>
+                <div className="card-body d-flex  align-items-center justify-content-center "  id='cardDesignAdd'>
+                  <div className='cardContent' id='addCardcontent' >
+                    <h5 className="card-title fw-bold m-0 ">
+                    <i class="bi bi-plus-circle-dotted"></i>
+                      Add More
+                      
+                      </h5>
+                  </div>
+
+                  {/* <div className='actions'>
+                    <a href="#" className="btn btn-secondary">
+                      Update
+                    </a>
+                    <a href="#" className="btn btn-danger">
+                      Delete
+                    </a>
+                  </div> */}
+                </div>
+              </div>
         </div>
       </div>
     </>
